ee756c70454e79e45d940a5f6fcee749c2fed5c9
NIHAL276482 <nihalchakradhri9@gmail.com>
2025-05-13 14:20:33 +0530

    Update env.py

--- env.py
+++ env.py
@@ -7,7 +7,7 @@ API_ID = os.getenv("API_ID", "25305140").strip()
 API_HASH = os.getenv("API_HASH", "6d19044c96341ccb3da294f878df7659").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8197669660:AAEkomDdO3exXbljezRZuKX5nU8BJnTRe5Y").strip()
 DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://DSTWARRIORDB:DSTMONGODB@dstdb.y4cayps.mongodb.net/?retryWrites=true&w=majority&appName=Dstdb").strip() # Not a necessary variable anymore but you can add to get stats
-MUST_JOIN = os.getenv("MUST_JOIN", "t.me/drsudo")
+MUST_JOIN = os.getenv("MUST_JOIN", "drsudo")
 
 if not API_ID:
     raise SystemExit("No API_ID found. Exiting...")
